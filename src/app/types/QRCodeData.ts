aab23758dae5d71a74ab3d8bee298a23fe635361
= <=>
2023-05-14 01:27:23 -0400

    Added defaults for all options that can't be null

--- src/app/types/QRCodeData.ts
+++ src/app/types/QRCodeData.ts
@@ -3,20 +3,20 @@ import { QRCodeErrorCorrectionLevel } from "qrcode"
 import { QRCodeElementType } from 'angularx-qrcode/public-api';
 
 export class QRCodeData extends IModel{
-  public allowEmptyString?: boolean;
+  public allowEmptyString: boolean = false;
   public alt?: string;
   public ariaLabel?: string;
-  public colorDark?: string;
-  public colorLight?: string;
-  public cssClass?: string;
-  public elementType?: QRCodeElementType;
-  public errorCorrectionLevel?: QRCodeErrorCorrectionLevel;
+  public colorDark: string = '#000000ff';
+  public colorLight: string = '#ffffffff';
+  public cssClass: string = 'qrcode';
+  public elementType: QRCodeElementType = 'canvas';
+  public errorCorrectionLevel: QRCodeErrorCorrectionLevel = 'M';
   public imageSrc?: string;
   public imageHeight?: number;
   public imageWidth?: number;
-  public margin?: number;
+  public margin: number = 4;
   public qrData: string = 'ddavie.com';
-  public scale?: number;
+  public scale: number = 4;
   public title?: string;
-  public width?: number;
+  public width: number = 300;
 }
